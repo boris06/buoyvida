e7117934ef7ba5ba1b1d777139ecbe01ec3f1630
Damir Dezeljin <damir.dezeljin@dezo.org>
2016-08-31 22:28:01 +0200

    Add DB connection properties.

---
+++ mbp_buoy_vida/config.py
@@ -0,0 +1,9 @@
+# Description:
+#  This is the DB connection configuration file. To get scripts working, the
+#  below class DbConfig should be properly filled.
+
+class DbConfig:
+  username = None
+  password = None
+  host = None
+  db = None
